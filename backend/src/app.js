667df0d40e7177cb2dafe1e40ac2bc80825ed339
Keoma Kevin Baudin <keoma4@gmail.com>
2024-03-25 00:41:22 -0300

    add swagger

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const swaggerUi = require('swagger-ui-express');
 
 const requestLoggerMiddleware = require('./middlewares/request-logger');
 const productRouter = require('./routes/product-router');
+const { swaggerSpec, swaggerUiOptions } = require('./docs/swagger');
 
 const app = express();
 
@@ -10,6 +12,13 @@ const app = express();
 app.use(bodyParser.json());
 app.use(requestLoggerMiddleware);
 app.use(productRouter);
+
+app.get('/api-docs/swagger.json', (req, res) => res.json(swaggerSpec));
+app.use(
+  '/api-docs',
+  swaggerUi.serveFiles(null, swaggerUiOptions),
+  swaggerUi.setup(null, swaggerUiOptions)
+);
 
 // falta integrar o frontend com o backend
 
---
+++ backend/src/docs/swagger.js
@@ -0,0 +1,23 @@
+const swaggerJsdoc = require('swagger-jsdoc');
+
+const swaggerJsdocOptions = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Petshop API',
+      description: 'Petshop store API',
+      version: '1.0.0',
+    },
+  },
+  apis: ['./src/routes/*.js'], // files containing annotations as above
+};
+
+const swaggerSpec = swaggerJsdoc(swaggerJsdocOptions);
+
+const swaggerUiOptions = {
+  swaggerOptions: {
+    url: '/api-docs/swagger.json',
+  },
+};
+
+module.exports = { swaggerSpec, swaggerUiOptions };
